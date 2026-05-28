952abb50dd395bdb8aa8d5324c416ce74326260d
Nguyễn Duy Hà <ha8893536@gmail.com>
2026-05-28 18:56:49 +0700

    Update package.json

--- backend/server.js
+++ backend/server.js
@@ -224,10 +224,10 @@ app.use("/api/ai", aiRoutes);
 app.use("/", sitemapRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 	app.get(/.*/, (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
 	});
 }
 
