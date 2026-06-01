909f89024f45137f29430aaecb51d545d35ec5fe
Nguyễn Duy Hà <ha8893536@gmail.com>
2026-06-02 00:22:11 +0700

    Fix server listen log

--- backend/server.js
+++ backend/server.js
@@ -271,7 +271,7 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(PORT, () => {
-	console.log("Server is running on http://localhost:" + PORT);
+	console.log(`Server is running on port ${PORT}`);
 	connectDB();
 });
 
